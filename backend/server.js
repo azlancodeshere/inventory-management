96a25588113dc5e6439ce51d25637be66cb6df97
azlancodeshere <azlan.codes@gmail.com>
2026-09-25 22:42:17 +0530

    fixing server.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,14 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://inventory-management-omega-lemon.vercel.app"
+        ],
         credentials: true,
     })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
