781b42f1954b03ccde37c97b9ffdb208d329b7c4
azlancodeshere <azlan.codes@gmail.com>
2026-09-27 00:58:16 +0530

    fix api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-export const Base_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";
+export const Base_URL =
+    import.meta.env.VITE_BASE_URL ||
+    "http://localhost:5000/api";
 
 const api = axios.create({
     baseURL: Base_URL,
     withCredentials: true,
-    headers:{
-        "content-type": "application/json"
-    }
-})
-
-
+});
 
 export default api;
